b1d033e015e285353fb19a715c34bb18fbc8884a
aryan ganesh <108381175+Aryan-Ganesh@users.noreply.github.com>
2024-08-30 21:57:18 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// declares a class Hello World 
+public class HelloWorld{
+    // source code is under main
+    public static void main(final String[] args){
+        // prints hello world
+        System.out.println("Hello World");
+    }
+}
